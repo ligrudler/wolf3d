c924a0fea3ecb1beb233bde34fd26339ad615585
Quentin LOUISIADE <qlouisia@e2r1p13.42.fr>
2019-11-27 20:52:56 +0100

    reconfiguration du bresenham et ajout de la fonction pour dessiner des carrees

--- src/utils.c
+++ src/utils.c
@@ -6,12 +6,11 @@
 /*   By: qlouisia <qlouisia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/27 16:19:49 by lgrudler          #+#    #+#             */
-/*   Updated: 2019/11/27 20:50:59 by qlouisia         ###   ########.fr       */
+/*   Updated: 2019/11/27 20:52:48 by qlouisia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/wolf3d.h"
-#include <stdio.h>
 
 void	init_bres(t_line *bre, t_pt *p1, t_pt *p2)
 {
@@ -35,10 +34,7 @@ int	ft_drawline(t_pt s1, t_pt s2, t_sdl *sdl)
 	{
 		SDL_RenderDrawPoint(sdl->renderer, s1.x, s1.y);
 		if (s1.x == s2.x && s1.y == s2.y)
-		{
-			printf("[%d,%d]\n",s1.x,s1.y);
 			return (0);
-		}
 		bre.e2 = bre.err;
 		if (bre.e2 > -(bre.dx))
 		{
