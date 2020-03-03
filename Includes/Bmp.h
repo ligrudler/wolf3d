a0529501755b65454bd84329b19cb06b993fa2b5
Lise Grudler <lgrudler@student.42.fr>
2020-03-03 18:09:53 +0100

    debut norme bmp.h

--- Includes/Bmp.h
+++ Includes/Bmp.h
@@ -7,22 +7,22 @@
 
 typedef struct	s_bmpfiles
 {
-	int size;
-	uint16_t width;
-	uint16_t height;
-	int bpp;
-	int BPP;
-	int px_data_offset;
-	int header_size;
-	int nb_color_palette;
-	int compression;
-	int image_size;
-	int important_color_nb;
-	uint32_t *palette;
-	uint32_t *data;
+	int			size;
+	uint16_t	width;
+	uint16_t	height;
+	int			bpp;
+	int			BPP;
+	int			px_data_offset;
+	int			header_size;
+	int			nb_color_palette;
+	int			compression;
+	int			image_size;
+	int			important_color_nb;
+	uint32_t	*palette;
+	uint32_t	*data;
 }				t_bmp;
 
-int create_palette(char *tmp, t_bmp *bmp);
-void format_data8bit(char *tmp, t_bmp *bmp);
-int exit8bit(t_bmp *bmp);
+int				create_palette(char *tmp, t_bmp *bmp);
+void			format_data8bit(char *tmp, t_bmp *bmp);
+int				exit8bit(t_bmp *bmp);
 #endif
