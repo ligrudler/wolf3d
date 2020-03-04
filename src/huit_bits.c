98fb98e241b60b5f1496520c4233f8940043cd29
Quentin LOUISIADE <qlouisia@e2r3p9.42.fr>
2020-03-04 19:21:13 +0100

    norme in huit/bit

--- src/huit_bits.c
+++ src/huit_bits.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   huit_bits.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgrudler <lgrudler@student.42.fr>          +#+  +:+       +#+        */
+/*   By: qlouisia <qlouisia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 13:33:21 by qlouisia          #+#    #+#             */
-/*   Updated: 2020/03/04 16:12:56 by lgrudler         ###   ########.fr       */
+/*   Updated: 2020/03/04 19:20:24 by qlouisia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ uint32_t	convert_8bitscolor(char r, char g, char b)
 	int nb;
 	int value;
 
-	value = ((255 & 0xFF) << 24) | //alpha
-			(((int)r & 0xFF) << 16) | //red
-			(((int)g & 0xFF) << 8) | //green
-			(((int)b & 0xFF) << 0); //blue
+	value = ((255 & 0xFF) << 24) |
+			(((int)r & 0xFF) << 16) |
+			(((int)g & 0xFF) << 8) |
+			(((int)b & 0xFF) << 0);
 	return (value);
 }
 
@@ -47,7 +47,6 @@ void		format_data8bit(char *tmp, t_bmp *bmp)
 		y--;
 	}
 }
-/* Color Format in raw data : R B G 0  */
 
 int			create_palette(char *tmp, t_bmp *bmp)
 {
@@ -59,7 +58,8 @@ int			create_palette(char *tmp, t_bmp *bmp)
 	tmp += bmp->header_size + 14;
 	if (bmp->nb_color_palette == 0)
 		bmp->nb_color_palette = 256;
-	if (!(bmp->palette = (uint32_t *)malloc(sizeof(uint32_t) * bmp->nb_color_palette)))
+	if (!(bmp->palette = (uint32_t *)malloc(sizeof(uint32_t) *
+	bmp->nb_color_palette)))
 		return (0);
 	while (i < bmp->nb_color_palette * 4)
 	{
